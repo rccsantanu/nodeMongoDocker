240045e5099b4c6232fc349f06ee3ce034497b9a
santanu aich <santanu@pitangent.com>
2024-05-02 17:22:38 +0530

    Add jenkins

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Initialize DB 1
+// Initialize DB 1 2
 require("./initDB")();
 
 const ProductRoute = require("./Routes/Product.route");
