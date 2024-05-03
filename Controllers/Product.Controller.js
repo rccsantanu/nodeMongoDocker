d483a242dabfb59b8d12dc5d0f9c7139678bffeb
santanu aich <santanu@pitangent.com>
2024-05-03 16:28:27 +0530

    Add Jenlins file

--- Controllers/Product.Controller.js
+++ Controllers/Product.Controller.js
@@ -109,7 +109,7 @@ module.exports = {
 
   testing: async (req, res, next) => {
     try {
-      res.send("Hello, this is a static message! v-03, santanu 01");
+      res.send("Hello, this is a static message! v-03, santanu 02");
     } catch (error) {
       console.log(error.message);
     }
